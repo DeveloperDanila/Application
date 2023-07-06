8be76ff021b18d989770b19a173f479c93a10c63
Classic Engineer <classicdev@yandex.ru>
2023-07-06 23:07:18 +0300

    [app1]: Hello World now have two langs! But how to extend it?

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
+import java.util.Locale;
+
+/**
+ * Prints Hello World depens on locale
+ * GoogleHelp:
+ * 1. https://howtodoinjava.com/java/date-time/how-to-get-current-user-locale-in-java/
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        var currentLocale = Locale.getDefault();
+        var currentLanguage = currentLocale.getLanguage();
+        switch(currentLanguage) {
+            case "en" -> System.out.println("Hello, world!");
+            case "ru" -> System.out.println("Привет, мир!");
+        }
     }
 }
