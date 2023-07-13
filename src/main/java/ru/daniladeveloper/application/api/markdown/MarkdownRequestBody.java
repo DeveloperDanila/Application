bb3217cb7096ecd4e47279c21b6a129836f58902
Classic Engineer <classicdev@yandex.ru>
2023-07-13 16:41:05 +0300

    remove extra modules

--- src/main/java/ru/daniladeveloper/application/api/markdown/MarkdownRequestBody.java
+++
@@ -1,9 +0,0 @@
-package ru.daniladeveloper.application.api.markdown;
-
-import lombok.Data;
-
-@Data
-public class MarkdownRequestBody {
-
-    private String content;
-}
--- src/main/java/ru/daniladeveloper/application/domain/markdown/download/ImageDownloadException.java
+++
@@ -1,9 +0,0 @@
-package ru.daniladeveloper.application.domain.markdown.download;
-
-
-public class ImageDownloadException extends Exception {
-
-    public ImageDownloadException(Exception e) {
-        super(e);
-    }
-}
